27015a0264a9106c653dd773e577e1382364ac8e
Arti <artem.kort@stiftsgymnasium.de>
2026-03-08 15:11:07 +0100

    Make the method public

--- src/main/java/com/thirst/AnimationControllers.java
+++ src/main/java/com/thirst/AnimationControllers.java
@@ -9,7 +9,7 @@ import software.bernie.geckolib.animation.RawAnimation;
 import software.bernie.geckolib.constant.DefaultAnimations;
 
 public class AnimationControllers {
-    static AnimationController<? extends Unit> WalkFuncIdle(String functionName, Predicate<Void> playFunc) {
+    public static AnimationController<? extends Unit> WalkFuncIdle(String functionName, Predicate<Void> playFunc) {
         return new AnimationController<Unit>("Function/Walk/Idle", state -> {
             // controllers.add(DefaultAnimations.genericWalkIdleController());
             if (playFunc.test(null)) {
