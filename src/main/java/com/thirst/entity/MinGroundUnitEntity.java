2c0cda43fc03c14626af684a49a80e6862be5e2f
Arti <artem.kort@stiftsgymnasium.de>
2026-03-09 12:49:43 +0100

    Minor changes

--- src/main/java/com/thirst/entity/MinGroundUnitEntity.java
+++ src/main/java/com/thirst/entity/MinGroundUnitEntity.java
@@ -1,7 +1,5 @@
 package com.thirst.entity;
 
-import org.jspecify.annotations.NonNull;
-
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.EntityType;
 // import net.minecraft.entity.ai.goal.EscapeDangerGoal;
@@ -11,16 +9,8 @@ import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
-import software.bernie.geckolib.util.GeckoLibUtil;
 
 public class MinGroundUnitEntity extends GroundUnit {
-    private final @NonNull AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
-
-    @Override
-    public @NonNull AnimatableInstanceCache getAnimatableInstanceCache() {
-        return this.cache;
-    }
 
     public MinGroundUnitEntity(EntityType<? extends PathAwareEntity> type, World world) {
         super(type, world);
