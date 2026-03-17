5addd77fe2f5f6dedefff765a23e3439f9fd507a
Arti <artem.kort@stiftsgymnasium.de>
2026-03-17 17:51:18 +0100

    Refactor FormationBase.java

--- src/main/java/com/thirst/systems/formation/types/FormationBase.java
+++ src/main/java/com/thirst/systems/formation/types/FormationBase.java
@@ -32,34 +32,56 @@ public abstract class FormationBase {
     protected final List<UUID> psychedelics = new ArrayList<>();
     protected boolean initialized = false;
     public UUID uuid;
+    protected BlockPos targetLocation;
+    protected FormationState state = FormationState.BUILDING;
+    protected int meleeNumber = 0;
+    protected int rangedNumber = 0;
+    protected int groundNumber = 0;
+    protected int psychedelicNumber = 0;
+    protected boolean allInPosition = false;
+    protected boolean positionCheck = true;
+
+    // Getters
 
     public String toString() {
         return this.getClass() + ". Type: " + this.getType() + ".";
-    }
-
-    public abstract String getType();
-
-    protected BlockPos targetLocation;
-
-    public void setTargetLocation(BlockPos targetLocation) {
-        this.targetLocation = targetLocation;
     }
 
     public BlockPos getTargetLocation() {
         return targetLocation;
     }
 
-    protected FormationState state = FormationState.BUILDING;
-
     public FormationState getState() {
         return state;
     }
 
-    protected int meleeNumber = 0;
-    protected int rangedNumber = 0;
-    protected int groundNumber = 0;
-    protected int psychedelicNumber = 0;
-    protected boolean allInPosition = false;
+    public boolean areAllInPosition() {
+        return allInPosition;
+    }
+
+    public boolean getPositionCheck() {
+        return positionCheck;
+    }
+
+    protected static ServerWorld getServerWorld(MinecraftServer server) {
+        return server.getOverworld();
+    }
+
+    // Setters
+
+    public void setTargetLocation(BlockPos targetLocation) {
+        this.targetLocation = targetLocation;
+    }
+    // Abstract methods
+
+    public abstract String getType();
+
+    protected abstract Vec3d calculateSlot(Entity leader, int indexInGroup, int groupUnitCount, UnitType type);
+
+    protected abstract void electNewLeader(MinecraftServer server);
+
+    // Codec
+
     public static final Codec<FormationBase> DISPATCH_CODEC = Codec.STRING
             .dispatch(
                     FormationBase::getType,
@@ -68,6 +90,66 @@ public abstract class FormationBase {
                                 .get(ThirstId.registryKey(ModRegistries.FORMATION_REGISTRY_KEY, id)).codec();
                     });
 
+    // Util methods
+
+    public int getIdInGroup(Unit unit) {
+        UUID uuid = unit.getUuid();
+        switch (unit.getUnitType()) {
+            case MELEE:
+                return melees.indexOf(uuid);
+            case RANGED:
+                return rangers.indexOf(uuid);
+            case GROUND:
+                return grounds.indexOf(uuid);
+            case PSYCHEDELIC:
+                return psychedelics.indexOf(uuid);
+        }
+        // Will never happen
+        return -2;
+    }
+
+    protected int getCountByType(UnitType type) {
+        switch (type) {
+            case MELEE:
+                return melees.size();
+            case RANGED:
+                return rangers.size();
+            case GROUND:
+                return grounds.size();
+            case PSYCHEDELIC:
+                return psychedelics.size();
+        }
+        return -1;
+    }
+
+    protected void checkInPosition(Unit unit) {
+        positionCheck &= unit.inPosition;
+    }
+
+    protected double findValidY(World world, double x, double z, double referenceY) {
+        // 1. Get the highest block at this X/Z (The Surface)
+        int topY = world.getTopY(Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, (int) x, (int) z);
+
+        // 2. If the surface is too far away vertically (e.g., a cliff),
+        // we want to check blocks closer to the Leader's height first.
+        // This prevents units from snapping to the roof of a house when the leader is
+        // inside.
+        BlockPos referencePos = new BlockPos((int) x, (int) referenceY, (int) z);
+
+        // Check if there is a floor near the reference height (within 3 blocks up or
+        // down)
+        for (int offset : new int[] { 0, 1, -1, 2, -2, 3, -3 }) {
+            BlockPos checkPos = referencePos.up(offset);
+            if (world.getBlockState(checkPos).isSolidBlock(world, checkPos) &&
+                    world.isAir(checkPos.up())) {
+                return checkPos.getY() + 1.0; // Return top of the solid block
+            }
+        }
+
+        // 3. Fallback: If no local floor is found, use the surface heightmap
+        return topY;
+    }
+
     public FormationBase(BlockPos target, List<UUID> members, String state, boolean initialized) {
         this.targetLocation = target;
         this.members = members != null ? new ArrayList(members) : new ArrayList<>();
@@ -75,26 +157,14 @@ public abstract class FormationBase {
         this.initialized = initialized;
     }
 
-    public boolean areAllInPosition() {
-        return allInPosition;
-    }
-
-    protected boolean positionCheck = true;
-
-    public boolean getPositionCheck() {
-        return positionCheck;
-    }
+    // Update logic
 
     protected void spawnInitialMembers(ServerWorld world, BlockPos target, int count) {
         for (int i = 0; i < count; i++) {
-            // 1. Create your Unit (replace with your actual EntityType)
-            ;
 
-            // 2. Spread them out slightly so they don't 'explode' outwards from collision
             double offsetX = (world.random.nextDouble() - 0.5) * 4.0;
             double offsetZ = (world.random.nextDouble() - 0.5) * 4.0;
 
-            // 3. Find a valid Y (preventing spawning in the floor/ceiling)
             double spawnX = target.getX() + offsetX;
             double spawnZ = target.getZ() + offsetZ;
             double spawnY = world.getTopY(Heightmap.Type.MOTION_BLOCKING, (int) spawnX, (int) spawnZ);
@@ -102,22 +172,12 @@ public abstract class FormationBase {
                     BlockPos.ofFloored(spawnX, spawnY, spawnZ), SpawnReason.TRIGGERED, false, false);
             unit.refreshPositionAndAngles(spawnX, spawnY, spawnZ, world.random.nextFloat() * 360F, 0);
 
-            // 4. Critical: Assign the formation before spawning
             this.members.add(unit.getUuid());
 
-            // 5. Actually spawn the entity into the world
             world.spawnEntity(unit);
 
         }
     }
-
-    protected abstract Vec3d calculateSlot(Entity leader, int indexInGroup, int groupUnitCount, UnitType type);
-
-    protected static ServerWorld getServerWorld(MinecraftServer server) {
-        return server.getOverworld();
-    }
-
-    protected abstract void electNewLeader(MinecraftServer server);
 
     protected void updateUnitNumbersAndLists(ServerWorld world) {
         meleeNumber = 0;
@@ -158,40 +218,6 @@ public abstract class FormationBase {
 
         });
         members.removeAll(removeList);
-    }
-
-    public int getIdInGroup(Unit unit) {
-        UUID uuid = unit.getUuid();
-        switch (unit.getUnitType()) {
-            case MELEE:
-                return melees.indexOf(uuid);
-            case RANGED:
-                return rangers.indexOf(uuid);
-            case GROUND:
-                return grounds.indexOf(uuid);
-            case PSYCHEDELIC:
-                return psychedelics.indexOf(uuid);
-        }
-        // Will never happen
-        return -2;
-    }
-
-    protected int getCountByType(UnitType type) {
-        switch (type) {
-            case MELEE:
-                return melees.size();
-            case RANGED:
-                return rangers.size();
-            case GROUND:
-                return grounds.size();
-            case PSYCHEDELIC:
-                return psychedelics.size();
-        }
-        return -1;
-    }
-
-    protected void checkInPosition(Unit unit) {
-        positionCheck &= unit.inPosition;
     }
 
     public void stopAttack(MinecraftServer server) {
@@ -237,29 +263,4 @@ public abstract class FormationBase {
         }
     }
 
-    protected double findValidY(World world, double x, double z, double referenceY) {
-        // 1. Get the highest block at this X/Z (The Surface)
-        int topY = world.getTopY(Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, (int) x, (int) z);
-
-        // 2. If the surface is too far away vertically (e.g., a cliff),
-        // we want to check blocks closer to the Leader's height first.
-        // This prevents units from snapping to the roof of a house when the leader is
-        // inside.
-        BlockPos referencePos = new BlockPos((int) x, (int) referenceY, (int) z);
-
-        // Check if there is a floor near the reference height (within 3 blocks up or
-        // down)
-        for (int offset : new int[] { 0, 1, -1, 2, -2, 3, -3 }) {
-            BlockPos checkPos = referencePos.up(offset);
-            if (world.getBlockState(checkPos).isSolidBlock(world, checkPos) &&
-                    world.isAir(checkPos.up())) {
-                return checkPos.getY() + 1.0; // Return top of the solid block
-            }
-        }
-
-        // 3. Fallback: If no local floor is found, use the surface heightmap
-        return topY;
-    }
-
-    // Inside FormationBase or its subclasses
 }
